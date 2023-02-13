15d7bbe0d7d3353664603210b1dc6878c1630797
ReshitkoM <73237232+ReshitkoM@users.noreply.github.com>
2023-02-13 23:24:22 +0300

    initial sketch

---
+++ test.py
@@ -0,0 +1,33 @@
+import pika, sys, os
+
+from recognizer import recognize
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        f = open('test.ogg', 'wb')
+        f.write(body)
+        f.close()
+        res = recognize(body)
+        print("123")
+        print(res)
+
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
