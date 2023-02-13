cb2b5c1ca5dadc005116180ebff50d3a45f2b18c
ReshitkoM <73237232+ReshitkoM@users.noreply.github.com>
2023-02-13 23:26:19 +0300

    add rpc

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='rpc_queue')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
@@ -14,10 +14,17 @@ def main():
         f.write(body)
         f.close()
         res = recognize(body)
+        ch.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         properties.correlation_id),
+                     body=res)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
         print("123")
         print(res)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='rpc_queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
